f3856055390e3dee01894c9fb0f403d050d6664b
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-05-04 10:32:47 +0300

    Created the project and its structure

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AlWaddahClinic.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("AlWaddahClinic.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AlWaddahClinic.ServerAPI"));
+
+await builder.Build().RunAsync();
+
