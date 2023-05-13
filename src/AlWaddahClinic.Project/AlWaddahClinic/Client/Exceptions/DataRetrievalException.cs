c12aef59b50fa4def57315539f1841de64f2c9b6
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-05-13 20:40:09 +0300

    Designed the add new patient page

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Exceptions/DataRetrievalException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AlWaddahClinic.Client.Exceptions
+{
+	public class DataRetrievalException : Exception
+	{
+		public DataRetrievalException(string message) : base(message)
+		{
+		}
+	}
+}
+
