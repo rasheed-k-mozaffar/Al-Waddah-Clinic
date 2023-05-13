4c9c74de1bf4152cfb7112552eef08d2b47bd4ce
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-05-13 20:38:13 +0300

    Added the All Patients page

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/Patients/All.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace AlWaddahClinic.Client.Pages.Patients
+{
+    public partial class All : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public IPatientsService PatientsService { get; set; } = null!;
+
+        ApiResponse<IEnumerable<PatientSummaryDto>> patients = new();
+
+
+        private void GoToAddPatient()
+        {
+            NavigationManager.NavigateTo("/patients/add");
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            patients = await PatientsService.GetAllPatients();
+        }
+    }
+}
+
