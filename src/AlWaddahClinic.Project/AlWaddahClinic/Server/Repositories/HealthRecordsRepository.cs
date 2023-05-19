99c76d37bc26540c89374aad6fd90249d686b332
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-05-19 18:22:03 +0300

    Added the health records repository

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Server/Repositories/HealthRecordsRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AlWaddahClinic.Server.Data;
+
+namespace AlWaddahClinic.Server.Repositories
+{
+    public class HealthRecordsRepository : IHealthRecordsRepository
+    {
+        private readonly ClinicDbContext _context;
+
+        public HealthRecordsRepository(ClinicDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<HealthRecord>> GetHealthRecordsForPatientAsync(int patientId)
+        {
+            var healthRecords = await _context.HealthRecords.Where(hr => hr.PatientId == patientId).ToListAsync();
+
+            if (!healthRecords.Any())
+            {
+                throw new NotFoundException("No health records were found for this patient");
+            }
+
+            return healthRecords;
+        }
+
+        public async Task<HealthRecord> GetHealthRecordByIdAsync(int patientId, int recordId)
+        {
+            var healthRecord = await _context.HealthRecords.FirstOrDefaultAsync(hr => hr.PatientId == patientId);
+
+            if (healthRecord == null)
+            {
+                throw new NotFoundException("Health record was not found");
+            }
+
+            return healthRecord;
+        }
+
+        public async Task AddHealthRecordAsync(int patientId, HealthRecord model)
+        {
+            var patient = await _context.Patients.FindAsync(patientId);
+
+            if (patient == null)
+            {
+                throw new NotFoundException("Patient was not found");
+            }
+
+
+            await _context.HealthRecords.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveHealthRecord(int recordId)
+        {
+            var healthRecord = await _context.HealthRecords.FindAsync(recordId);
+
+            if (healthRecord == null)
+            {
+                throw new NotFoundException("Health record was not found");
+            }
+
+            _context.HealthRecords.Remove(healthRecord);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Server/Repositories/Interfaces/IHealthRecordsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AlWaddahClinic.Server.Repositories.Interfaces
+{
+	public interface IHealthRecordsRepository
+	{
+		Task<IEnumerable<HealthRecord>> GetHealthRecordsForPatientAsync(int patientId);
+		Task<HealthRecord> GetHealthRecordByIdAsync(int patientId, int recordId);
+		Task RemoveHealthRecord(int recordId);
+		Task AddHealthRecordAsync(int patientId, HealthRecord model);
+	}
+}
+
