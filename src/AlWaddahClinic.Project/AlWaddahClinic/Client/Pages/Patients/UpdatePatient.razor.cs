cf53443c92a37bb87d2495ceac982b51651969a3
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-05-24 22:25:52 +0300

    Created the files for the update patient

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/Patients/UpdatePatient.razor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace AlWaddahClinic.Client.Pages.Patients
+{
+	public partial class UpdatePatient : ComponentBase
+	{
+		//TODO: inject the required objects
+
+		[Parameter] public int PatientId { get; set; }
+
+        //TODO: implement the methods
+        protected override async Task OnInitializedAsync()
+        {
+			throw new NotImplementedException();
+        }
+
+        private async Task Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
