8cda01974adacb96bf2fe90416e8c49f3e444566
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-05-31 23:09:35 +0300

    Designed and implemented all the appointment related pages and logic

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/Appointments/ListAppointments.razor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using AlWaddahClinic.Client.Components;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AlWaddahClinic.Client.Pages.Appointments
+{
+	public partial class ListAppointments : ComponentBase
+	{
+		[Inject]
+		public NavigationManager NavigationManager { get; set; } = default!;
+
+		[Inject]
+		public IAppointmentsService AppointmentsService { get; set; } = default!;
+
+		[Inject]
+		public IDialogService DialogService { get; set; } = default!;
+
+		private List<AppointmentSummaryDto> appointments;
+
+		private bool _isBusy = true;
+		private string _errorMessage = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+			try
+			{
+				appointments = (await AppointmentsService.GetAllAppointmentsAsync()).Value.ToList();
+				_isBusy = false;
+			}
+			catch(DomainException ex)
+			{
+				_errorMessage = ex.Message;
+			}
+        }
+
+		private async Task OpenRemoveDialog(int id)
+		{
+			var parameters = new DialogParameters();
+			parameters.Add("Header", "Confirm Removal");
+			parameters.Add("Content", "Do you really want to remove this appointment? Please note that once the process has completed it cannot be inverted");
+			parameters.Add("ButtonText", "Remove");
+			parameters.Add("Color", Color.Error);
+
+			var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
+
+			var dialog = DialogService.Show<Dialog>("Delete", parameters, options);
+			var result = await dialog.Result;
+
+			if(!result.Canceled)
+			{
+				await RemoveAsync(id);
+			}
+		}
+
+		private void GoToUpdateAppointment(string patientName, int appointmentId)
+		{
+			NavigationManager.NavigateTo($"/appointments/reschedule/{patientName}/{appointmentId}");
+		}
+
+		private void GoToPastAppointments()
+		{
+			NavigationManager.NavigateTo("/appointments/past");
+		}
+
+		private async Task RemoveAsync(int id)
+		{
+			try
+			{
+				await AppointmentsService.RemoveAppointmentAsync(id);
+				var itemToRemove = appointments.SingleOrDefault(a => a.Id == id);
+				appointments.Remove(itemToRemove);
+			}
+			catch(DomainException ex)
+			{
+				_errorMessage = ex.Message;
+			}
+		}
+    }
+}
+
---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/Appointments/UpdateAppointment.razor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AlWaddahClinic.Client.Pages.Patients;
+using System.Reflection;
+using Microsoft.AspNetCore.Components;
+
+namespace AlWaddahClinic.Client.Pages.Appointments
+{
+	public partial class UpdateAppointment : ComponentBase
+	{
+		[Inject]
+		public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Inject]
+        public IPatientsService PatientsService { get; set; } = default!;
+
+        [Inject]
+		public IAppointmentsService AppointmentsService { get; set; } = default!;
+
+		[Parameter] public int AppointmentId { get; set; }
+        [Parameter] public string PatientName { get; set; } = default!;
+
+        private AppointmentDto oldAppointment = new();
+        private AppointmentUpdateDto model = new();
+
+        private string _errorMessage = string.Empty;    
+        private bool _isMakingRequest = false;
+        private bool _isBusy = true;
+        private TimeSpan? time = new TimeSpan();
+        private DateTime? appointmentDay = DateTime.Now;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                oldAppointment = (await AppointmentsService.GetAppointmentByIdAsync(AppointmentId)).Value;
+                _isBusy = false;
+            }
+            catch(NotFoundException ex)
+            {
+                _errorMessage = ex.Message;
+            }
+        }
+
+        private async Task UpdateAsync()
+        {
+            _errorMessage = string.Empty;
+            _isMakingRequest = true;
+            try
+            {
+                model.StartAt = new DateTime(appointmentDay.Value.Year, appointmentDay.Value.Month, appointmentDay.Value.Day, time.Value.Hours, time.Value.Minutes, 0);
+                await AppointmentsService.UpdateAppointmentAsync(AppointmentId ,model);
+                NavigationManager.NavigateTo("/appointments");
+            }
+            catch (DomainException ex)
+            {
+                _errorMessage = ex.Message;
+            }
+
+            _isMakingRequest = false;
+        }
+
+        private void GoBack()
+        {
+            NavigationManager.NavigateTo("/appointments");
+        }
+    }
+}
+
