73cc35516330c02c1e0736ab91110e3ddee47a30
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-06-12 16:24:44 +0300

    Designed the form for registering a new clinic

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/user/RegisterClinic.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace AlWaddahClinic.Client.Pages.User
+{
+	public partial class RegisterClinic : ComponentBase
+	{
+		[Inject]
+		public NavigationManager NavigationManager { get; set; } = default!;
+
+		private RegisterClinicDto model = new();
+
+		private string _country = "🇸🇾 Syria";
+		private string _errorMessage = string.Empty;
+		private bool _isMakingRequest = false;
+
+		private DateTime? graduationDate;
+		private RegisterUserDto userModel = new();
+
+		private async Task RegisterAsync()
+		{
+
+		}
+
+	}
+}
+
