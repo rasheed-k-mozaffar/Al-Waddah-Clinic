69b28b479b6559cb2cfff57e7363a0560ee17cc8
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-06-16 21:49:35 +0300

    Fixed the empty search query bug

--- src/AlWaddahClinic.Project/AlWaddahClinic/Client/Components/PatientSearchDialog.razor.cs
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Components/PatientSearchDialog.razor.cs
@@ -1,35 +1,38 @@
-﻿	using System;
+﻿using System;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace AlWaddahClinic.Client.Components
 {
-	public partial class PatientSearchDialog : ComponentBase
-	{
-		[Inject]
-		public IPatientsService PatientsService { get; set; } = default!;
+    public partial class PatientSearchDialog : ComponentBase
+    {
+        [Inject]
+        public IPatientsService PatientsService { get; set; } = default!;
 
-		[Inject]
-		public NavigationManager NavigationManager { get; set; } = default!;
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
 
-		[Parameter] public List<PatientSummaryDto>? PatientsToSearch { get; set; }
+        [Parameter] public List<PatientSummaryDto>? PatientsToSearch { get; set; }
 
         ApiResponse<IEnumerable<PatientSummaryDto>> searchResults = new();
 
         private string _searchText = string.Empty;
-		private bool _isBusy = false;
+        private bool _isBusy = false;
 
-		private void GoToSearchResults(string searchQuery)
-		{
-			NavigationManager.NavigateTo($"/patients/search/{searchQuery}");
-		}
+        private void GoToSearchResults(string searchQuery)
+        {
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                NavigationManager.NavigateTo($"/patients/search/{searchQuery}");
+            }
+        }
 
         private void Cancel() => MudDialog.Cancel();
 
         private void GoToPatient(Guid patientId)
-		{
-			NavigationManager.NavigateTo($"/patients/{patientId}");
-		}
-	}
+        {
+            NavigationManager.NavigateTo($"/patients/{patientId}");
+        }
+    }
 }
 
