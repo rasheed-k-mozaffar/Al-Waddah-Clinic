fd35aa8105517d8945c76d0ac44ec54af64aa944
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-07-01 18:32:29 +0300

    Added smart insights to the health record related pages

--- src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/Patients/Records/AddHealthRecord.razor.cs
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/Pages/Patients/Records/AddHealthRecord.razor.cs
@@ -5,12 +5,20 @@ namespace AlWaddahClinic.Client.Pages.Patients.Records
 {
 	public partial class AddHealthRecord : ComponentBase
 	{
+		#region Injected Services
 		[Inject]
-		public NavigationManager NavigationManager { get; set; } = null!;
+		public NavigationManager NavigationManager { get; set; } = default!;
 
 		[Inject]
-		public IPatientsService PatientsService { get; set; } = null!;
-		[Inject] IHealthRecordsService HealthRecordsService { get; set; } = default!; 
+		public IPatientsService PatientsService { get; set; } = default!;
+
+		[Inject]
+		public IHealthRecordsService HealthRecordsService { get; set; } = default!;
+
+		[Inject]
+		public IAiService AiService { get; set; } = default!;
+		#endregion
+
 		[Parameter] public Guid PatientId { get; set; }
 
 		private ApiResponse<PatientDto> result = new();
@@ -18,22 +26,37 @@ namespace AlWaddahClinic.Client.Pages.Patients.Records
 		private HealthRecordCreateDto model = new();
 		private List<NoteCreateDto>? notes = new();
 
+		#region Smart Insights Collections
+		private List<string>? patientSuggestions = new();
+		private List<string>? relatedCases = new();
+		private List<string>? suggestedTests = new();
+		private List<string>? caseInsight = new();
+		#endregion
+
 		private string _errorMessage = string.Empty;
 		private string _noteTitle = string.Empty;
+		private string InsightsPannelText => _isPreparingInsights ? "Loading..." : "Get Ai Generated Insights Regarding Your Patient's Case! Just Click The Lightbulb";
 
-        protected override async Task OnInitializedAsync()
-        {
+		private bool _isInsightReady = false;
+		private bool _isPreparingInsights = false;
+
+		protected override async Task OnInitializedAsync()
+		{
 			result = await PatientsService.GetPatientById(PatientId);
 			patient = result.Value;
-        }
+		}
 
-		//TODO: Implement this method
-        private async Task AddRecord()
+		private async Task AddRecord()
 		{
 			_errorMessage = string.Empty;
 			try
 			{
 				model.Notes = notes;
+				//Mapping the Ai insights.
+				model.PatientSuggestion = patientSuggestions;
+				model.RelatedMedicalCases = relatedCases;
+				model.MedicalCaseInsight = caseInsight;
+				model.SuggestedMedicalTests = suggestedTests;
 				await HealthRecordsService.AddRecordAsync(PatientId, model);
 				NavigationManager.NavigateTo($"/patients/{PatientId}");
 			}
@@ -45,7 +68,7 @@ namespace AlWaddahClinic.Client.Pages.Patients.Records
 
 		private void AddNote()
 		{
-			if(!string.IsNullOrEmpty(_noteTitle))
+			if (!string.IsNullOrEmpty(_noteTitle))
 			{
 				notes.Add(new NoteCreateDto { Title = _noteTitle });
 			}
@@ -53,9 +76,31 @@ namespace AlWaddahClinic.Client.Pages.Patients.Records
 			_noteTitle = string.Empty;
 		}
 
+		private async Task GetInsights()
+		{
+			CaseDto _case = new()
+			{
+				message = model.Description
+			};
+			//Pass the description from the health record to the Ai Service
+			if (!string.IsNullOrEmpty(_case.message))
+			{
+				_isInsightReady = false;
+				_isPreparingInsights = true;
+
+				patientSuggestions = (await AiService.GetSuggestionsForPatientAsync(_case)).Value.ToList();
+				relatedCases = (await AiService.GetRelatedMedicalCasesAsync(_case)).Value.ToList();
+				suggestedTests = (await AiService.GetSuggestedMedicalTestsAsync(_case)).Value.ToList();
+				caseInsight = (await AiService.GetCaseInsightsAsync(_case)).Value.ToList();
+
+				_isInsightReady = true;
+				_isPreparingInsights = false;
+			}
+		}
+
 		private void GoBack()
 		{
-			NavigationManager.NavigateTo($"/patients/patient/{PatientId}");
+			NavigationManager.NavigateTo($"/patients/{PatientId}");
 		}
 	}
 }
