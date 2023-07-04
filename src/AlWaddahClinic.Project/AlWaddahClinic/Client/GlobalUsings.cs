707c59f17be47e0b4de321a7b3af057babda8959
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-07-04 22:52:46 +0300

    Added the upcoming appointments component to the dashboard

--- src/AlWaddahClinic.Project/AlWaddahClinic/Client/GlobalUsings.cs
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Client/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using AlWaddahClinic.Client.Services;
 global using AlWaddahClinic.Client.Services.Interfaces;
 global using AlWaddahClinic.Client.Exceptions;
 global using AlWaddahClinic.Shared.ApiResponses;
+global using AlWaddahClinic.Client.UiServices;
