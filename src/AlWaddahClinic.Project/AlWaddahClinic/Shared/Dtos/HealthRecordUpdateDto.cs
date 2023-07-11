20e719831baaf3434bfdc628d0c3e5eab8036e2e
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-07-11 21:14:01 +0300

    Added properties for the smart insights

--- src/AlWaddahClinic.Project/AlWaddahClinic/Shared/Dtos/HealthRecordUpdateDto.cs
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Shared/Dtos/HealthRecordUpdateDto.cs
@@ -5,6 +5,10 @@ namespace AlWaddahClinic.Shared.Dtos
 	{
         public string Description { get; set; }
         public List<NoteUpdateDto>? Notes { get; set; }
+        public List<string>? PatientSuggestion { get; set; }
+        public List<string>? SuggestedMedicalTests { get; set; }
+        public List<string>? MedicalCaseInsight { get; set; }
+        public List<string>? RelatedMedicalCases { get; set; }
     }
 }
 
