7e9a20007a0438cbbdbcd1b6f6569b20a1a613bb
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-08-03 23:11:13 +0300

    Created a new repository for the payments model, and registered it

---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Server/Repositories/Interfaces/IPaymentsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AlWaddahClinic.Server.Repositories.Interfaces
+{
+	public interface IPaymentsRepository
+	{
+		Task CreatePaymentAsync(Guid healthRecordId, Payment payment);
+		Task RemovePaymentAsync(Guid paymentId);
+	}
+}
+
---
+++ src/AlWaddahClinic.Project/AlWaddahClinic/Server/Repositories/PaymentsRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AlWaddahClinic.Server.Data;
+using AlWaddahClinic.Server.Options;
+
+namespace AlWaddahClinic.Server.Repositories
+{
+    public class PaymentsRepository : IPaymentsRepository
+	{
+        private readonly ClinicDbContext _context;
+        private readonly IdentityOptions _options;
+
+        public PaymentsRepository(ClinicDbContext context, IdentityOptions options)
+        {
+            _context = context;
+            _options = options;
+        }
+
+        public Task CreatePaymentAsync(Guid healthRecordId, Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePaymentAsync(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
